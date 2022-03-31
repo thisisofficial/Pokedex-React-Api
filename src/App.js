9fb24ad4bd78499c6b0bd637a941e176683c9356
thisisofficial <thisisofficiallyofficial@gmail.com>
2022-03-30 20:10:38 -0600

    Innitial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {PokeInfo} from './PokeInfo.js';
+import {Loader} from './loadercard.js';
+import {Error} from './error.js';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Loader/>} />
+        <Route path="/Pokemon/:name" element={<PokeInfo/>} />
+        <Route path="*" element={<Error/>}/>
+      </Routes>
+    </Router>
   );
 }
 
